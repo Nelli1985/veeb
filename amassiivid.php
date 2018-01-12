221bcaa7f6b2d35ba6ea9a6e1595fddff42d82e4
Nelli85 <NewLife2010>
2018-01-12 08:53:41 +0200

    Mõlemad funktsioonid tehtud... vahepealne committimine läks meelest

---
+++ amassiivid.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 1/12/2018
+ * Time: 8:47 AM
+ */
+
+$opilane = array(
+    'eesnimi' =>'Mart',
+    'perenimi' => 'Lepp',
+    'vanus' => 15,
+    'klass' =>9
+);
+
+echo '<pre>';
+print_r($opilane);
+echo '<pre>';
+
+foreach ($opilane as $vaartus) {
+    echo $vaartus.'<br />';
+}
+
+foreach ($opilane as $element => $vaartus) {
+    echo $element.' - '.$vaartus.'<br />';
+
+}
