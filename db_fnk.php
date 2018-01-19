d88467ef814160d7f028236aeb70543b1ce7d38a
Nelli85 <NewLife2010>
2018-01-19 13:06:02 +0200

    Ühendus loodud

---
+++ db_fnk.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 1/19/2018
+ * Time: 12:30 PM
+ */
+
+require_once 'db_conf.php'; // nõuame konfiguratsioonifaili sisu kasutamist
+
+// andmebaasi serveriga ühendamine ja
+// andmebaasi valik
+function yhendus() {
+    $yhendus = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    if(!$yhendus){
+        echo 'Puudub ühendus andmebaasi serveriga<br/>'; // kasutaja rahustamiseks kuvatakse ekraanile, kui ühendust ei toimu
+        echo mysqli_connect_error().'<br/>';
+        echo mysqli_connect_errno().'<br/>';
+        return false; // tähendab funktsioon yhendus ei saanud oma töö tehtud
+    } else {
+        echo 'Ühendus andmebaasiga on olemas. <br/>';
+        return $yhendus; // tagastab ühenduse, et seda saaks kasutada
+    }
+}
