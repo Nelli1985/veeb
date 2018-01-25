47a45707e68fa9a1af9553200430059898c60d23
Nelli85 <NewLife2010>
2018-01-25 11:26:13 +0200

    Loodud värvilise teksti konstruktor

--- tekstiTest.php
+++ tekstiTest.php
@@ -7,12 +7,28 @@
  */
 require_once 'tekst.php';
 // loome teksti objekt
-$minuTekst = new tekst();
 // määrame tekstile konkreetne sisu
-$minuTekst->maaraTekst('Tere Maailm!');
+$minuTekst = new tekst('Tere Maailm!');
 // vaatame, mis on objekti sees
 echo '<pre>';
 print_r($minuTekst);
 echo '</pre>';
 // prindime välja
 $minuTekst->prindiTekst();
+// loome veel ühe objekti
+// ja määrame sisu
+$mustTekst = new tekst('Must tekst');
+// vaatame, mis on objekti sees
+echo '<pre>';
+print_r($mustTekst);
+echo '</pre>';
+// prindime välja
+$mustTekst->prindiTekst();
+require_once 'vtekst.php';
+// loome värvilise objekti
+// ja määrame sisu
+$punaneTekst = new vtekst('Punane tekst', '#FF0000');
+echo '<pre>';
+print_r($punaneTekst);
+echo '</pre>';
+$punaneTekst->prindiTekst();
---
+++ vtekst.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 25.01.2018
+ * Time: 11:10
+ */
+require_once 'tekst.php';
+class vtekst extends tekst
+{
+    // lisame värvi ka
+    var $varv = '';
+    /**
+     * vtekst constructor.
+     * @param string $varv
+     */
+    public function __construct($sone, $varv)
+    {
+        parent::__construct($sone);
+        $this->maaraVarv($varv);
+    }
+    function maaraVarv($varv){
+        $this->varv = $varv;
+    }
+}
